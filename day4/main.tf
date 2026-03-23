44e105fdcd5c399c8488186cce27b6fb992b8a5d
pushpalmahajan05 <pushpalmahajan@gmail.com>
2026-03-23 09:42:06 +0000

    Formatted Terraform files

--- day4/main.tf
+++ day4/main.tf
@@ -10,13 +10,13 @@ terraform {
 provider "docker" {}
 
 module "nginx1" {
-  source          = "./modules/nginx_container"
-  container_name  = "nginx1"
-  container_port  = 8081
+  source         = "./modules/nginx_container"
+  container_name = "nginx1"
+  container_port = 8081
 }
 
 module "nginx2" {
-  source          = "./modules/nginx_container"
-  container_name  = "nginx2"
-  container_port  = 8082
+  source         = "./modules/nginx_container"
+  container_name = "nginx2"
+  container_port = 8082
 }
